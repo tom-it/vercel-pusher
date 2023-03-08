ba532ced52e68b575323c30f80f42059ebe361c3
Thomas Lang <thomaslang@MacBook-Pro-van-Thomas.local>
2023-03-08 21:03:31 +0100

    test

--- pages/api/channel-events.js
+++ pages/api/channel-events.js
@@ -1,17 +1,18 @@
 const Channels = require('pusher');
 
 const {
-    '1565295': appId,
-    '105a23cb70c7843e3a00': key,
-    '30f65df9044563a2b21c': secret,
-    'us2': cluster,
+    APP_ID: appId,
+    KEY: key,
+    SECRET: secret,
+    CLUSTER: cluster,
 } = process.env;
 
+
 const channels = new Channels({
-    appId : '1565295',
-    key : '105a23cb70c7843e3a00',
-    secret : '30f65df9044563a2b21c',
-    cluster : 'us2',
+    appId,
+    key,
+    secret,
+    cluster,
 });
 
 module.exports = (req, res) => {
